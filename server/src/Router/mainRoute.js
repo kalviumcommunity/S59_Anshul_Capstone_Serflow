2beac9c9db7a92b7b575f34334f6f65ede39a0c5
Merlinkk <anshulkashyap2504@gmail.com>
2024-04-03 10:54:32 +0530

    GET REQUEST API ADDEDget request added along with folder structure

--- server/src/Router/mainRoute.js
+++ server/src/Router/mainRoute.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Get Request Recieved!');
+    res.send('Get Request Recieved!✅✅✅✅✅✅✅✅✅✅ ');
 })
 
 module.exports = router;
