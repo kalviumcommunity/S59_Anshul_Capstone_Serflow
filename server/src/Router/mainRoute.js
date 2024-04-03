850cb04ba9736d3c2217b02912e18e7f3d48d80e
Merlinkk <anshulkashyap2504@gmail.com>
2024-04-03 11:16:54 +0530

    codium ai revised changes

--- server/src/Router/mainRoute.js
+++ server/src/Router/mainRoute.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Get Request Recieved!✅✅✅✅✅✅✅✅✅✅ ');
+    res.status(200).send('Get Request Recieved!✅✅✅✅✅✅✅✅✅✅ ');
 })
 
 module.exports = router;
