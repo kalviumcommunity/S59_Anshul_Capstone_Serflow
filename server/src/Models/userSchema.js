fa29303dd5960e3d8a552c6ed994e60962c80631
Merlinkk <anshulkashyap2504@gmail.com>
2024-04-16 10:43:36 +0530

    login/signup functional

---
+++ server/src/Models/userSchema.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+
+const userSchema = new mongoose.Schema({
+    username: {
+      type: String,
+      required: true,
+      // unique: true,
+      // trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      // unique: true,
+      // trim: true,
+      // lowercase: true,
+    },
+      password: {
+      hash: String,
+      salt: String,
+    },
+  }, { timestamps: true });
+
+
+userSchema.methods.setPassword = function (password) {
+  this.password.salt = crypto.randomBytes(16).toString('hex');
+  this.password.hash = crypto
+    .pbkdf2Sync(password, this.password.salt, 1000, 64, 'sha512')
+    .toString('hex');
+};
+
+userSchema.methods.validatePassword = function (password) {
+  const hash = crypto
+    .pbkdf2Sync(password, this.password.salt, 1000, 64, 'sha512')
+    .toString('hex');
+  return this.password.hash === hash;
+};
+
+const User = mongoose.model('users', userSchema);
+
+module.exports = User;
+
+      
--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,12 @@
 require("dotenv").config()
 const express = require("express")
 const mainRouter = require("./Router/mainRoute")
+const authRouter = require("./Router/authRouter")
 const app = express()
-
+const cors = require('cors')
 app.use(express.json());
-
+app.use(cors())
+app.use("/auth", authRouter)
 app.use("/main", mainRouter)
 
 module.exports = app
