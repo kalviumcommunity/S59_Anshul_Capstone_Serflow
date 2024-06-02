d3ed194e3a3f8293b5b11804a21084b521c75e74
Merlinkk <anshulkashyap2504@gmail.com>
2024-06-02 20:50:05 +0530

    edited cors

--- server/src/app.js
+++ server/src/app.js
@@ -5,8 +5,24 @@ const authRouter = require("./Router/authRouter")
 const app = express()
 const cors = require('cors')
 app.use(express.json());
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://serflow.netlify.app/login',
+  'https://serflow.netlify.app'
+];
+
+
 app.use(cors({
-    origin: 'http://localhost:5173', // Replace with your frontend's origin
+    origin:(origin, callback) => {
+      if (!origin) return callback(null, true);
+  
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true // Allow cookies to be sent with requests
   }));
 app.use("/auth", authRouter)
